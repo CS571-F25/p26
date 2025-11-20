488a991ae3d9aabfd2022ff3cf9c428ca3c580bf
Neil D'Souza <neilgregdsouza@gmail.com>
2025-11-19 22:50:24 -0600

    merge fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import NavigationBar from "./components/NavigationBar";
-import SearchPage from "./pages/SearchPage";
-import SavedProfilesPage from "./pages/SavedProfilesPage";
+import SearchPage from "./Pages/SearchPage";
+import SavedProfilesPage from "./Pages/SavedProfilesPage";
 import SettingsPage from "./pages/SettingsPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
 
+  const [favoritePeople, setFavoritePeople] = useState([
+  ]);
+
+  function addPerson(newPerson) {
+    console.log("adding person: " + newPerson.id);
+    setFavoritePeople(prev => [...prev, newPerson]);
+  }
+
+  function deletePerson(id) {
+    console.log("deleting person: " + id);
+    setFavoritePeople(prev => prev.filter(person => person.id !== id));
+  }
+
   return (
   <div className={darkMode ? "bg-dark text-light" : "bg-light text-dark"} style={{ minHeight: "100vh", width: "100vw" }}>
     <Router>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/saved" element={<SavedProfilesPage />} />
+        <Route path="/" element={
+          <SearchPage 
+          addFavoritePerson={addPerson}
+          deleteFavoritePerson={deletePerson}
+          favorites={favoritePeople}/>
+          } />
+        <Route path="/saved" element={
+          <SavedProfilesPage 
+          people={favoritePeople}
+          addFavoritePerson={addPerson}
+          deleteFavoritePerson={deletePerson}
+          favorites={favoritePeople}/>
+          } />
         <Route path="/settings" element={<SettingsPage darkMode={darkMode} setDarkMode={setDarkMode} />} />
     </Routes>
     </Router>
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,77 @@
-export default function SearchPage() {
+import { useState } from "react";
+import TextEntry from "../Components/TextEntry";
+import { Button, Card } from "react-bootstrap";
+import PersonList from "../Components/PersonList";
+
+function SearchPage(props) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [people, setPeople] = useState([
+        {id:1, firstName:"Jane", lastName:"Doe"},
+        {id:2, firstName:"John", lastName:"Doe"},
+        {id:3, firstName:"Jane", lastName:"Smith"},
+        {id:4, firstName:"John", lastName:"Smith"},
+        {id:5, firstName:"Ryan", lastName:"Khalloqi"},
+        {id:6, firstName:"Neil", lastName:"D'Souza"},
+    ]);
+    const [searchedFirstName, setSearchedFirstName] = useState("");
+    const [searchedLastName, setSearchedLastName] = useState("");
+
+    let filteredPeople = people.filter(person => {
+        const f = cleanString(searchedFirstName);
+        const l = cleanString(searchedLastName);
+
+        return (
+            f === cleanString(person.firstName).substring(0, f.length) &&
+            l === cleanString(person.lastName).substring(0, l.length)
+        );
+    });
+
+    function search() {
+        setSearchedFirstName(firstName);
+        setSearchedLastName(lastName);
+    }
+
+    function cleanString(str) {
+        return str.trim().toLowerCase();
+    }
+
+    function setFavorite(person, bool) {
+        if (!bool) {
+            props.addFavoritePerson(person);
+        } else {
+            props.deleteFavoritePerson(person.id)
+        }
+    }
+
     return (
-        <div style={{ padding: "2rem" }}>
-            <h2>Search Page</h2>
-        </div>
-    );
+    <><Card>
+        <TextEntry
+        label="First Name"
+        value={firstName}
+        onChange={setFirstName}
+        />
+        <TextEntry
+        label="Last Name"
+        value={lastName}
+        onChange={setLastName}
+        />
+        <Button
+        style={{
+            margin: 20,
+            backgroundColor:"green",
+            outlineColor:"red"
+        }}
+        onClick={search}
+        >Search!</Button>
+    </Card>
+    <PersonList
+    people={filteredPeople}
+    toggleFavorite={setFavorite}
+    initialState={false}
+    favorites={props.favorites}
+    />
+    </>)
 }
+
+export default SearchPage
